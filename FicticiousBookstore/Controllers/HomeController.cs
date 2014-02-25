127dda64ed0502141fa7827879b27f7ab9765c24
Matthew Banz <mabanz@ncsu.edu>
2014-02-24 23:40:30 -0700

    Implemented the Contact page and improved styles.

--- FicticiousBookstore/Controllers/HomeController.cs
+++ FicticiousBookstore/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using FicticiousBookstore.Models;
+using System.Net;
+using System.Net.Mail;
 using System.Web.Mvc;
 
 namespace FicticiousBookstore.Controllers
@@ -22,9 +21,39 @@ namespace FicticiousBookstore.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var fromAddress = "ficticiousbooksellers@gmail.com";
+
+                using (var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAddress, "fiction1234")
+                })
+                {
+                    using (var message = new MailMessage(fromAddress, fromAddress)
+                        {
+                            Subject = model.Subject,
+                            Body = model.Email + " sent you the following message:\n\n" + model.Message
+                        })
+                    {
+                        smtp.Send(message);
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
     }
 }
